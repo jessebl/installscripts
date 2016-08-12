41f4f56e11efc86190f41ba4494e8241be147a4f
kantsdog <j.bl@openmailbox.org>
2016-08-12 12:53:35 -0700

    run after torrent completion This file will replace the stuff/nas_rsync script

---
+++ torrent.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#This script is to run on completion of torrents, and act on directories that group the torrents by label. To that end, make sure that you label your torrents and have your torrent client set to append the label to the savepath.
+
+tmovies=/home/jesse/Downloads/Torrents/Movies
+
