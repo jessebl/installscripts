932a3477841995e2234ecddc1915531700c4dc82
kantsdog <j.bl@openmailbox.org>
2016-08-13 01:11:40 -0700

    Added filebot for TV Had issues with calling the TV directories "TV Shows" instead of "TV"; kept getting "Failed to process group: {} => [/home/jesse/Downloads/Torrents/TV, /home/jesse/installscripts/Shows/*]". Will look into that next commit.

--- torrent.sh
+++ torrent.sh
@@ -6,15 +6,15 @@
 # Set source directories
 s=/home/jesse/Downloads/Torrents/ # Allows for shorthanding the movies and tv files, since they should both have the same parent folder
 smovies=$s\Movies
-stv=$s\TV\ Shows
+stv=$s"TV"
 
 # Set destination directories
 d=/home/jesse/test/targ/
 dmovies=$d\Movies
-dtv=$d\TV\ Shows
+dtv=$d'TV'
 
 # Use filebot to write to destination with proper formatting, from the source directories
 ## Movies
 ### Movies are copied to $dmovies inside directories named with the movie titles. Actual files are named "title-year"
 filebot -rename --action copy $smovies\/* --format "$dmovies/{n}\ ({y})/{n}\ ({y})" -non-strict
-#filebot -rename --action copy $stv\/* --format "{home}/test/targ/TV Shows/{n}/Season {s}/{n}-{s00e00}-{t} " -non-strict
+filebot -rename --action copy $stv\/* --format "$dtv/{n}\ ({y})/Season {s}/{n}-{s00e00}-{t} " -non-strict
