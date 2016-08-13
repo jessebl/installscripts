1b9f3a011572e27c258c8ad071cd47e6d2eeb15d
kantsdog <j.bl@openmailbox.org>
2016-08-13 00:36:52 -0700

    Tweaked filebot output format filebotting movies works!! Just changed the output format to look nicer when including the year.

--- torrent.sh
+++ torrent.sh
@@ -16,4 +16,4 @@ dtv=$d\TV\ Shows
 # Use filebot to write to destination with proper formatting, from the source directories
 ## Movies
 ### Movies are copied to $dmovies inside directories named with the movie titles. Actual files are named "title-year"
-filebot -rename --action copy $smovies\/* --format "{home}/test/targ/Movies/{n}-{y}/{n}-{y}" -non-strict
+filebot -rename --action copy $smovies\/* --format "{home}/test/targ/Movies/{n}\ ({y})/{n}\ ({y})" -non-strict
