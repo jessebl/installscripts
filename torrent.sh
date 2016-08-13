641b30b8d4dd876eded20237f2f62f25b5690809
kantsdog <j.bl@openmailbox.org>
2016-08-13 01:33:01 -0700

    Fixed "TV" -> "TV Shows" issue!! The problem was in line 20 where $stv was being passed with the spaces inside not being escaped. Double quotes around $stv fixed the issue.

--- torrent.sh
+++ torrent.sh
@@ -6,15 +6,15 @@
 # Set source directories
 s=/home/jesse/Downloads/Torrents/ # Allows for shorthanding the movies and tv files, since they should both have the same parent folder
 smovies=$s\Movies
-stv=$s"TV"
+stv=$s'TV Shows'
 
 # Set destination directories
 d=/home/jesse/test/targ/
 dmovies=$d\Movies
-dtv=$d'TV'
+dtv=$d'TV Shows'
 
 # Use filebot to write to destination with proper formatting, from the source directories
 ## Movies
 ### Movies are copied to $dmovies inside directories named with the movie titles. Actual files are named "title-year"
 filebot -rename --action copy $smovies\/* --format "$dmovies/{n}\ ({y})/{n}\ ({y})" -non-strict
-filebot -rename --action copy $stv\/* --format "$dtv/{n}\ ({y})/Season {s}/{n}-{s00e00}-{t} " -non-strict
+filebot -rename --action copy "$stv"\/* --format "$dtv/{n}\ ({y})/Season {s}/{n}-{s00e00}-{t} " -non-strict
