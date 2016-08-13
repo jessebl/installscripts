8ca3b08c6403757547ebe2b338bfdb52ad093b7b
kantsdog <j.bl@openmailbox.org>
2016-08-12 23:34:02 -0700

    *actually* added dest. paths

--- torrent.sh
+++ torrent.sh
@@ -3,7 +3,11 @@
 #This script is to run on completion of torrents, and act on directories that group the torrents by label. To that end, make sure that you label your torrents and have your torrent client set to append the label to the savepath.
 
 # Set source directories
-t=/home/jesse/Downloads/Torrents/ # Allows for shorthanding the movies and tv files, since they should both have the same parent folder
-tmovies=$t\Movies
-ttv=$t\TV\ Shows
+s=/home/jesse/Downloads/Torrents/ # Allows for shorthanding the movies and tv files, since they should both have the same parent folder
+smovies=$t\Movies
+stv=$t\TV\ Shows
 
+# Set destination directories
+d=/media/jesse/boa/Videos/
+dmovies=$d\Movies
+dtv=$d\TV\ Shows
