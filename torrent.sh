a3c6c0316a09f91ee72624979412ad2f4b478eb8
kantsdog <j.bl@openmailbox.org>
2016-08-14 17:42:41 -0700

    Added support for second destination Made early-line edits to leave the option for the trailing slash in rsync source in the rsync line. The rest should be pretty self-explanatory.

--- torrent.sh
+++ torrent.sh
@@ -4,13 +4,13 @@
 # Dependencies: filebot
 
 # Set source directories
-s=/home/jesse/Downloads/Torrents/ # Allows for shorthanding the movies and tv files, since they should both have the same parent folder
-smovies=$s\Movies
+s=/home/jesse/Downloads/Torrents # Allows for shorthanding the movies and tv files, since they should both have the same parent folder
+smovies=$s\/Movies
 stv=$s'TV Shows'
 
 # Set destination directories
-d=/home/jesse/test/targ/
-dmovies=$d\Movies
+d=/home/jesse/test/targ
+dmovies=$d\/Movies
 dtv=$d'TV Shows'
 
 # Use filebot to write to destination with proper formatting, from the source directories
@@ -18,3 +18,9 @@ dtv=$d'TV Shows'
 ### Movies are copied to $dmovies inside directories named with the movie titles. Actual files are named "title-year"
 filebot -rename --action copy $smovies\/* --format "$dmovies/{n}\ ({y})/{n}\ ({y})" -non-strict
 filebot -rename --action copy "$stv"\/* --format "$dtv/{n}\ ({y})/Season {s}/{n}-{s00e00}-{t} " -non-strict
+
+# Use rsync to transfer from $d to the NAS
+
+d2=/mnt/nas-multimedia/jesse-videos
+
+rsync -avz --progress $d $d2
