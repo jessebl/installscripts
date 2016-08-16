5868f6de74e94f93c07c70ffce88451913875bec
kantsdog <j.bl@openmailbox.org>
2016-08-16 16:37:08 -0700

    Comments + re-added variable for 2nd rsync Tried to comment all lines well. Also re-added the variable as the primary argument for the second rsync line, for figuring out the problem.

--- torrent.sh
+++ torrent.sh
@@ -7,24 +7,24 @@
 mkdir -p logs
 
 # Set source directories
-s=/home/jesse/Downloads/Torrents # Allows for shorthanding the movies and tv files, since they should both have the same parent folder
-smovies=$s/Movies
-stv=$s/'TV Shows'
+s=/home/jesse/Downloads/Torrents # Allows for shorthanding the movies and tv files, since they should both have the same parent directory
+smovies=$s/Movies  # Source directory for movies
+stv=$s/"TV Shows" # Source directory for TV Shows
 
 # Set destination directories
-d=/media/jesse/boa/Videos
-dmovies=$d\/Movies
-dtv=$d/"TV\ Shows"
+d=/media/jesse/boa/Videos 
+dmovies=$d\/Movies # Destination directory for movies
+dtv=$d/"TV\ Shows" # Destination directory for TV Shows
 
 # Use filebot to write to destination with proper formatting, from the source directories
 ## Movies
 ### Movies are copied to $dmovies inside directories named with the movie titles. Actual files are named "title-year"
-filebot -rename --action copy $smovies\/* --format "$dmovies/{n}\ ({y})/{n}\ ({y})" -non-strict | tee logs/filebot1.txt
-filebot -rename --action copy "$stv"\/* --format "$dtv/{n}\ ({y})/Season {s}/{n}-{s00e00}-{t} " -non-strict | tee logs/filebot2.txt
+filebot -rename --action copy $smovies/* --format "$dmovies/{n}\ ({y})/{n}\ ({y})" -non-strict | tee logs/filebot1.txt # filebot copies media from $smovies to $dmovies with path renaming
+filebot -rename --action copy "$stv"/* --format "$dtv/{n}\ ({y})/Season {s}/{n}-{s00e00}-{t} " -non-strict | tee logs/filebot2.txt # filebot copies media from $stv to $dtv with path renaming
 
 # Use rsync to transfer from $d to the NAS
 
-d2=/mnt/nas-multimedia/jesse-videos
+d2=/mnt/nas-multimedia/jesse-videos # Secondary destination, transfer with rsync
 
-rsync -avz --progress $dmovies $d2 | tee logs/rsync1.txt
-rsync -az -vvv --progress --protect-args /media/jesse/boa/Videos/TV\ Shows $d2 | tee logs/rsync2.txt
+rsync -avz --progress $dmovies $d2 | tee logs/rsync1.txt # rsync transfers files from $dmovies to $d2
+rsync -az -vvv --progress --protect-args $dtv $d2 | tee logs/rsync2.txt # rsync transfers files from $dtv to $d2
