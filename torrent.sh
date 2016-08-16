154b7bfef3383d2cd60d48e717fafa6ad427f6ba
kantsdog <j.bl@openmailbox.org>
2016-08-15 19:12:50 -0700

    Added the rsync stuff and logging Problem with the rsync lines: had to hard-code the source for $dtv because rsync couldn't seem to deal with the space in "TV Shows" from $dtv. I tried escaping the spaces every way I could, even using echo in the argument. For now, this works though.

--- torrent.sh
+++ torrent.sh
@@ -3,24 +3,28 @@
 #This script is to run on completion of torrents, and act on directories that group the torrents by label. To that end, make sure that you label your torrents and have your torrent client set to append the label to the savepath.
 # Dependencies: filebot
 
+# Make log directory
+mkdir -p logs
+
 # Set source directories
 s=/home/jesse/Downloads/Torrents # Allows for shorthanding the movies and tv files, since they should both have the same parent folder
-smovies=$s\/Movies
-stv=$s'TV Shows'
+smovies=$s/Movies
+stv=$s/'TV Shows'
 
 # Set destination directories
-d=/home/jesse/test/targ
+d=/media/jesse/boa/Videos
 dmovies=$d\/Movies
-dtv=$d'TV Shows'
+dtv=$d/"TV\ Shows"
 
 # Use filebot to write to destination with proper formatting, from the source directories
 ## Movies
 ### Movies are copied to $dmovies inside directories named with the movie titles. Actual files are named "title-year"
-filebot -rename --action copy $smovies\/* --format "$dmovies/{n}\ ({y})/{n}\ ({y})" -non-strict
-filebot -rename --action copy "$stv"\/* --format "$dtv/{n}\ ({y})/Season {s}/{n}-{s00e00}-{t} " -non-strict
+filebot -rename --action copy $smovies\/* --format "$dmovies/{n}\ ({y})/{n}\ ({y})" -non-strict | tee logs/filebot1.txt
+filebot -rename --action copy "$stv"\/* --format "$dtv/{n}\ ({y})/Season {s}/{n}-{s00e00}-{t} " -non-strict | tee logs/filebot2.txt
 
 # Use rsync to transfer from $d to the NAS
 
 d2=/mnt/nas-multimedia/jesse-videos
 
-rsync -avz --progress $d $d2
+rsync -avz --progress $dmovies $d2 | tee logs/rsync1.txt
+rsync -az -vvv --progress --protect-args /media/jesse/boa/Videos/TV\ Shows $d2 | tee logs/rsync2.txt
