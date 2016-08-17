e8590ef7b8b25c3355239ee5ae5dab572e9ce53a
kantsdog <j.bl@openmailbox.org>
2016-08-16 19:51:12 -0700

    Actual fix

--- vimconfig.sh
+++ vimconfig.sh
@@ -2,7 +2,7 @@
 # This script sets up a new .vimrc, as well as Vundle for the plugin manager. The plugins include airline (with patched fonts).
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp /configs/.vimrc ~/.vimrc
+cp configs/.vimrc ~/.vimrc
 git clone https://github.com/powerline/fonts.git
 cd fonts
 ./install.sh
