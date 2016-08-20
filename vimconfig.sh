e9d696edc9c43d104fa4d5760b79b398cc6ecaf0
kantsdog <j.bl@openmailbox.org>
2016-08-20 14:38:29 -0700

    Adds comments, and comments out 2 lines
    Finally adds some documentation for vim converts. Also comments out
    manpager line (16) since it is not currently functional except for the
    current terminal emulator session. Finally, comments out sed line (18)
    that changes the Molokai's colorscheme to allow for transparency.

--- vimconfig.sh
+++ vimconfig.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#/bin/bash
 # This script sets up a new .vimrc, as well as Vundle for the plugin manager. The plugins include airline (with patched fonts).
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -8,4 +8,4 @@ git clone https://github.com/powerline/fonts.git
 rm -rf fonts
 vi +PluginInstall +qall
 export MANPAGER="/bin/sh -c \"col -b | vim -c 'set ft=man ts=8 nomod nolist nonu noma' -\""
-sed -i[no-transparency.vim] 's/ctermfg=252 ctermbg=233/ctermfg=252\ ctermbg=none/g' ~/.vim/bundle/molokai/colors/molokai.vim
+sed -i[no-transparency.vim] 's/ctermfg=252 ctermbg=233/ctermfg=252\ ctermbg=none\ \"ctermbg=233/g' ~/.vim/bundle/molokai/colors/molokai.vim
