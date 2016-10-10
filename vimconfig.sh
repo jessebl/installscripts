78c85ba1f76b7883a09b08a8b02a73c003136d69
Jesse <bulsonjg@whitman.edu>
2016-10-10 14:53:39 -0700

    Fixes bug with Vundle conditional
    .vim files are actually directories, so I changed the -f flag to -d flag
    for directories

--- vimconfig.sh
+++ vimconfig.sh
@@ -2,7 +2,7 @@
 # This script sets up a new .vimrc, as well as Vundle for the plugin manager. The plugins include airline (with patched fonts).
 
 # Download Vundle, a plug-in manager
-if [ -f $HOME/.vim/bundle/Vundle.vim]; then
+if [ -d $HOME/.vim/bundle/Vundle.vim ]; then
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 # Load in .vimrc (vim config file) from this repo
@@ -12,14 +12,14 @@ cp configs/.vimrc ~/.vimrc
 
 #If the patched fonts for vim-airline have not been installed (as evidenced by
 #the presence of the Ubuntu font), then install them.
-if [ -f $HOME/.local/share/fonts/'Ubuntu Mono derivative Powerline.ttf']; then
+if [ -f $HOME/.local/share/fonts/'Ubuntu Mono derivative Powerline.ttf' ]; then
 	git clone https://github.com/powerline/fonts.git
 	./fonts/install.sh
 	rm -rf fonts
 fi
 
 # Use Vundle to install all the programs called for by .vimrc
-vi +PluginInstall +qall
+vi +PluginClean +PluginUpdate +PluginInstall +qall 
 # Makes vim the default manpager (mostly for aesthetic purposes, but vim is such a functional home too)
 #echo 'export MANPAGER="col -b | vim -c 'set ft=man ts=8 nomod nolist nonu' -c 'nnoremap i <nop>' -"' >> ~/.bashrc
 # This line removes the background color from Molokai (the colorscheme set by .vimrc) to allow for transparent terminals. Commented out by default.
