fb0ef6fd58d7b420f842937734598fcf68bd59c3
kantsdog <j.bl@openmailbox.org>
2016-10-11 20:08:05 -0700

    Copies theme files to the user's themes directory

--- themeInstall.sh
+++ themeInstall.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
 # This script installs the theme ultra-flat-steftrikia from this repo.
+
+# Copy the theme files to the user's themes directory
+cp ultra-flat-steftrikia $HOME/.themes/ultra-flat-steftrikia
