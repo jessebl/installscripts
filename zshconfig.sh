003d3cc8868ef658c4137bc0726f862cb0fb01ac
kantsdog <j.bl@openmailbox.org>
2017-03-10 00:25:16 -0800

    Notifies user of need to logout after shell change

--- zshconfig.sh
+++ zshconfig.sh
@@ -13,4 +13,4 @@ sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/to
 # Copies over zsh config file
 cp configs/.zshrc $HOME/.zshrc
 echo "The zsh settings have been applied."
-echo "Open a new terminal to use zsh."
+echo "Open a new terminal to use zsh. You may have to log out and back in again."
