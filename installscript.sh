3acba9d13aa04b1cef1b6fb0b2618e113f5b767c
kantsdog <j.bl@openmailbox.org>
2017-03-12 19:54:38 -0700

    Cleans up and modernizes
    Clears out some old packages that I don't usually want. Also gets rid of repositories for programs I don't use anymore (Seafile > Dropbox). Also removes all things Calibre

--- installscript.sh
+++ installscript.sh
@@ -1,13 +1,5 @@
 #! /bin/bash
 
-user=jesse
-
-# Numix theme PPA
-apt-add-repository -y ppa:numix/ppa
-#Dropbox Keyserver (for securiry)
-apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
-#Dropbox Repository
-add-apt-repository -y "deb http://linux.dropbox.com/ubuntu $(lsb_release -sc) main"
 #Neovim repository
 add-apt-repository -y ppa:neovim-ppa/unstable
 #Seafile repositories
@@ -19,13 +11,7 @@ apt update
 apt -y upgrade
 
 # Install a bunch of packages
-apt install -y vim synapse redshift-gtk chromium-browser pepperflashplugin-nonfree synaptic unity-tweak-tool indicator-multiload keepass2 dropbox numix-icon-theme-circle compizconfig-settings-manager compiz-plugins-extra pithos clementine git cowsay xdotool ssh sshfs screenfetch mtpfs kodi steam libnotify-bin notifyosdconfig gimp calibre neovim zsh seafile-gui
+apt install -y vim synapse redshift-gtk chromium-browser pepperflashplugin-nonfree unity-tweak-tool indicator-multiload keepass2 compizconfig-settings-manager compiz-plugins-extra pithos clementine git cowsay xdotool ssh sshfs screenfetch mtpfs kodi libnotify-bin notifyosdconfig gimp neovim zsh seafile-gui
 
 # Clean up unnecessary dependencies
 apt -y autoremove
-
-# Set library path for Calibre (e-book software) (I am not sure if this works or not. Calibre command-line documentation is reallly terrible.)
-calibredb --library-path /home/$user/.calibre-library
-
-# Copy theme files from ultra-flat-steftrikia
-cp ultra-flat-steftrikia /usr/share/themes/Ultra\ Flat\
