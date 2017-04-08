ff3c7dbf86544d22e6a6373e6619e196a7f99952
kantsdog <j.bl@openmailbox.org>
2017-04-08 01:02:42 -0700

    Configures push behavior to 'simple'

--- gitconfig.sh
+++ gitconfig.sh
@@ -2,3 +2,7 @@
 git config --global user.name kantsdog
 git config --global user.email j.bl@openmailbox.org
 git config --global core.editor "vi"
+
+# By default, push only the current branch instead of all branches
+## All branches would be "matching" instead of "simple"
+git config --global push.default simple
