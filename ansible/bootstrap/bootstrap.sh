f915db9392dfba3ffae605eae82663a18579d630
kantsdog <j.bl@openmailbox.org>
2017-09-19 17:49:55 -0700

    Adds a few comments

--- ansible/bootstrap/bootstrap.sh
+++ ansible/bootstrap/bootstrap.sh
@@ -2,7 +2,9 @@
 
 ansible_playbook=$(dirname $0)/helper0.yml
 
+# Only works for Ubuntu systems
 if grep -q Ubuntu /etc/lsb-release; then
+  # Checks if Ansible is installed
   if ! dpkg -l ansible > /dev/null ; then
     echo "Installing Ansible..."
     sudo apt install ansible -y
@@ -10,6 +12,7 @@ if grep -q Ubuntu /etc/lsb-release; then
   echo "Ansible is already installed"
 fi	
 
+# Use Ansible to finish configuration
 echo "Running Ansible playbook $ansible_playbook"
 echo "Root access required for package management"
 ansible-playbook $ansible_playbook -K
