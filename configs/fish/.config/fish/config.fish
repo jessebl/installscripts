c2bf89b0feb023a88bb68286156c0bfa9e76966a
jessebl <j.bl@posteo.org>
2019-08-19 20:13:45 -0500

    Have fish fzf bindings use fd and rg; install fzf with ansible

--- configs/fish/.config/fish/config.fish
+++ configs/fish/.config/fish/config.fish
@@ -7,3 +7,18 @@ end
 fish_import_bash_aliases
 
 set fish_greeting
+
+if command -v fd > /dev/null
+    set -U FZF_CD_COMMAND "fd --type d"
+    set -U FZF_CD_WITH_HIDDEN_COMMAND "$FZF_CD_COMMAND --hidden"
+    set -U FZF_FIND_FILE_COMMAND "fd --type f . \$dir"
+    # Re-export to use above variable
+    set -U FZF_OPEN_COMMAND "$FZF_FIND_FILE_COMMAND"
+end
+if command -v rg > /dev/null
+    set -U FZF_FIND_FILE_COMMAND "rg --color auto --files \$dir"
+end
+set -U FZF_CD_OPTS "--preview='ls -l {}'"
+set -U FZF_PREVIEW_FILE_CMD "head -n 10"
+# Re-export to use above variable
+set -U FZF_OPEN_COMMAND "$FZF_FIND_FILE_COMMAND"
