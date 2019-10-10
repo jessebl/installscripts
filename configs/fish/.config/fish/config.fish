2e17894e0a6093f63bf4350db59e7a5dbdf2c69e
jessebl <j.bl@posteo.org>
2019-10-09 19:43:46 -0500

    Export environment variables instead of setting them globally

--- configs/fish/.config/fish/config.fish
+++ configs/fish/.config/fish/config.fish
@@ -9,24 +9,27 @@ if test -f $XDG_CONFIG_HOME/fish/local.fish
 	source $XDG_CONFIG_HOME/fish/local.fish
 end
 
+set -x PATH ~/.local/bin $PATH
+set -x XDG_DATA_DIRS ~/.local/share/flatpak/exports/share /var/lib/flatpak/exports/share $XDG_DATA_DIRS
+
 fish_import_bash_aliases
 
 set fish_greeting
 
 if command -v fd > /dev/null
-    set -U FZF_CD_COMMAND "fd --type d"
-    set -U FZF_CD_WITH_HIDDEN_COMMAND "$FZF_CD_COMMAND --hidden"
-    set -U FZF_FIND_FILE_COMMAND "fd --type f . \$dir"
+    set -x FZF_CD_COMMAND "fd --type d"
+    set -x FZF_CD_WITH_HIDDEN_COMMAND "$FZF_CD_COMMAND --hidden"
+    set -x FZF_FIND_FILE_COMMAND "fd --type f . \$dir"
     # Re-export to use above variable
-    set -U FZF_OPEN_COMMAND "$FZF_FIND_FILE_COMMAND"
+    set -x FZF_OPEN_COMMAND "$FZF_FIND_FILE_COMMAND"
 end
 if command -v rg > /dev/null
-    set -U FZF_FIND_FILE_COMMAND "rg --color auto --files \$dir"
+    set -x FZF_FIND_FILE_COMMAND "rg --color auto --files \$dir"
 end
-set -U FZF_CD_OPTS "--preview='ls -l {}'"
-set -U FZF_PREVIEW_FILE_CMD "head -n 10"
+set -x FZF_CD_OPTS "--preview='ls -l {}'"
+set -x FZF_PREVIEW_FILE_CMD "head -n 10"
 # Re-export to use above variable
-set -U FZF_OPEN_COMMAND "$FZF_FIND_FILE_COMMAND"
+set -x FZF_OPEN_COMMAND "$FZF_FIND_FILE_COMMAND"
 
 set -x GOPATH ~/code/go && mkdir -p $GOPATH
 set -x GO111MODULE on
