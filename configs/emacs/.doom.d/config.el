95ff725c6af62d5856eb030c03c31054fc872d0d
jessebl <j.bl@posteo.org>
2023-07-10 22:44:59 -0700

    Sets custom org directory

--- configs/emacs/.doom.d/config.el
+++ configs/emacs/.doom.d/config.el
@@ -29,7 +29,8 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq custom-org-dir "~/SeaDrive/My Libraries/Documents/40-49 Personal/")
+(setq org-directory (if (file-directory-p custom-org-dir) custom-org-dir "~/org/"))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
